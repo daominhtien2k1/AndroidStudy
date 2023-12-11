9878e9646052654e4ff66103078991875e44ca1d
Dao Minh Tien <minhtienk64hust@gmail.com>
2023-12-11 21:27:40 +0700

    state with multiple data

--- StateManagement/app/src/main/java/com/example/statemanagement/MainActivity.kt
+++ StateManagement/app/src/main/java/com/example/statemanagement/MainActivity.kt
@@ -22,8 +22,11 @@ class MainActivity : AppCompatActivity() {
         activityMainBinding.floatingActionButton.setOnClickListener {
             myViewModel.event(MyViewModel.MyViewModelEvent.GetMyData)
         }
+        activityMainBinding.floatingActionButton2.setOnClickListener {
+            myViewModel.event(MyViewModel.MyViewModelEvent.GetOtherData)
+        }
 
-        myViewModel.myData.observe(this) { state ->
+        myViewModel.myState.observe(this) { state ->
             when (state.myData) {
                 is MyViewModel.ApiResult.Init -> {
                     activityMainBinding.loading.visibility = View.GONE
@@ -38,6 +41,30 @@ class MainActivity : AppCompatActivity() {
                     activityMainBinding.loading.visibility = View.GONE
                     activityMainBinding.textView.visibility = View.VISIBLE
                     activityMainBinding.textView.text = "Hello World!"
+                }
+                is MyViewModel.ApiResult.Error -> {
+
+                }
+
+                else -> {}
+            }
+
+            // như thế này dù cập nhật lại myData thì cũng chạy lại otherData
+            // -> bị set lại textView2 và loading chưa kịp hiển thị visible thì đã set lại thành gone rồi
+            when (state.otherData) {
+                is MyViewModel.ApiResult.Init -> {
+                    activityMainBinding.loading.visibility = View.GONE
+                    activityMainBinding.textView2.visibility = View.VISIBLE
+                    activityMainBinding.textView2.text = "Init 2"
+                }
+                is MyViewModel.ApiResult.Loading -> {
+                    activityMainBinding.loading.visibility = View.VISIBLE
+                    activityMainBinding.textView2.visibility = View.INVISIBLE
+                }
+                is MyViewModel.ApiResult.Success -> {
+                    activityMainBinding.loading.visibility = View.GONE
+                    activityMainBinding.textView2.visibility = View.VISIBLE
+                    activityMainBinding.textView2.text = "Hello World 2!"
                 }
                 is MyViewModel.ApiResult.Error -> {
 
